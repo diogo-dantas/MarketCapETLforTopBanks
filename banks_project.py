0d276a0108fd7a99a87e3b95624ad388f9e42286
diogo-dantas <diogo.dantas@live.com>
2024-09-18 10:32:45 -0300

    Criada a função de log para o sistema.

--- banks_project.py
+++ banks_project.py
@@ -10,3 +10,13 @@ table_attribs = ['Name', 'MC_USD_Billion']
 db_name = 'Banks.db'
 table_name = 'Largest_banks'
 csv_path = './exchange_rate.csv'
+
+# função de registro das etapas do projeto
+
+def log_progress(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # obtém registro de data e hora atual
+    timestamp = now.strftime(timestamp_format) 
+    with open("./code_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
+
