e585fc6e22316bcfa79d6142b2f1ff0830687481
diogo-dantas <diogo.dantas@live.com>
2024-09-18 12:03:33 -0300

    Criada a função para extrair os dados da web page e armazena-los em um dataframe.

--- banks_project.py
+++ banks_project.py
@@ -20,3 +20,32 @@ def log_progress(message):
     with open("./code_log.txt","a") as f: 
         f.write(timestamp + ' : ' + message + '\n')
 
+
+def extract(url, table_attribs):
+    html_page = requests.get(url).text
+    data = BeautifulSoup(html_page, 'html.parser')
+
+    tables = data.find_all('tbody')
+    
+
+    # Checando se há tabelas na página da web
+    if not tables:
+        print("No tables found in the HTML.")
+        return pd.DataFrame()  # Retorna um dataframe vazio
+
+    rows = tables[2].find_all('tr')
+
+    df = pd.DataFrame()  # Inicializando um data frame vazio
+
+    for count, row in enumerate(rows[1:11], 1):  # Pula a linha de cabeçalho e extrai as 10 próximas linhas 
+        cols = row.find_all('td')
+        if len(cols) >= 0: 
+            data_dict = {
+                table_attribs[0]: cols[1].text.strip(),
+                table_attribs[1]: float(cols[2].text.strip().replace(',','')) # Converte string type para float
+            }
+            df = pd.concat([df, pd.DataFrame(data_dict, index=[0])], ignore_index=True)
+
+    return df
+
+    
