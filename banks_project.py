80dd715077e43d0b510636ef7a46201384e331ff
diogo-dantas <diogo.dantas@live.com>
2024-09-18 12:11:27 -0300

    Criada função para consultas ao banco de dados.

--- banks_project.py
+++ banks_project.py
@@ -65,3 +65,7 @@ def load_to_csv(df, csv_path): #armazena o dataframe em csv
 def load_to_db(df, sql_connection, table_name): #armazena os dados no banco de dados
     df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
 
+def run_query(query_statement, sql_connection):
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
